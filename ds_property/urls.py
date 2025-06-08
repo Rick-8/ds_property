229b50f655147ff3dd875942086f679cb1100af6
Rick-8 <npopbwls@students.codeinstitute.net>
2025-06-08 09:51:17 +0100

    Add manifest.json for PWA

--- ds_property/urls.py
+++ ds_property/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.views.generic import TemplateView
 from django.conf.urls.static import static
 
 urlpatterns = [
@@ -30,7 +31,7 @@ urlpatterns = [
     path("management/", include("management.urls")),
     path('', include('pwa.urls')),
     path('webpush/', include('webpush.urls')),
-
+    path('manifest.json', TemplateView.as_view(template_name="manifest.json", content_type='application/json'), name='manifest'),
 ]
 
 
